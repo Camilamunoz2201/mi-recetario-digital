5b37023107fbf7c18e63dc5a25effbdfe01e1f0d
Camilamunoz2201 <alxcmr18@gmail.com>
2025-08-26 22:26:58 -0500

    Archivo que permite conectar con java todos los archivos markdown creados incialmente

---
+++ data/recipes.js
@@ -0,0 +1,19 @@
+[
+  {
+    "slug": "pasta_carbonara",
+    "title": "Pasta Carbonara",
+    "file": "recetas/pasta_carbonara.md",
+    "image": "img/pasta carbonara.jpg",
+    "tags": ["pasta","italiano"],
+    "excerpt": "Pasta cremosa con panceta, yemas y queso parmesano."
+  },
+  {
+    "slug": "arepas_rellenas",
+    "title": "Arepas Rellenas",
+    "file": "recetas/arepas_rellenas.md",
+    "image": "img/arepa rellena queso.jpg",
+    "tags": ["maíz","rápido"],
+    "excerpt": "Arepas crujientes rellenas de queso o tu relleno favorito."
+  },
+  
+]
